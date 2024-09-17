58667d84fc60b33ffbefc1332e7bf11fc5ccd758
Evan Senter (614925) <614925@swl.local>
2024-09-17 08:50:59 -0400

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int inputNumber;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter a whole number");
+        inputNumber = scan.nextInt();
+
+        if (inputNumber % 2 == 0) {
+            System.out.println("Your number is even.");
+        }
+        else
+            System.out.println("Your number is odd");
+        }
     }
-}
